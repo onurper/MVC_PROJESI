041e0721a618197388b50f485068f388b932d24c
Onur PER <75972534+onurper@users.noreply.github.com>
2021-07-19 10:33:40 +0300

    MVC_PROJESI

---
+++ MVC_PROJESI/BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using DataAccesLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> rep = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return rep.List();
+        }
+        public void CategoryAddBL(Category p)
+        {
+            if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51)
+            {
+                // hata mesajı
+            }
+            else
+            {
+                rep.Insert(p);                
+            }
+        }
+    }
+}
---
+++ MVC_PROJESI/DataAccesLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Abstract
+{
+    public interface ICategoryDal:IRepository<Category>
+    {
+        List<Category> List();
+
+        void Insert(Category category);
+
+        void Update(Category category);
+
+        void Delete(Category category);
+    }
+}
